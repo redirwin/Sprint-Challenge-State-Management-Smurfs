0c7aece72df89e1370ace9d8e09f4b2ea3204f27
Dave Irwin <david@redirwin.com>
2019-07-26 09:28:56 -0700

    Sucessfully making axios call and grabbing data.

--- smurfs/src/store/actions/index.js
+++ smurfs/src/store/actions/index.js
@@ -1,8 +1,18 @@
 import axios from "axios";
 
 export const FETCH_SMURFS_START = "FETCH_SMURFS_START";
+export const FETCH_SMURFS_SUCCESS = "FETCH_SMURFS_SUCCESS";
+export const FETCH_SMURFS_FAILURE = "FETCH_SMURFS_FAILURE";
 
 export const getSmurfs = () => dispatch => {
   console.log("In action!");
   dispatch({ type: FETCH_SMURFS_START });
+  axios
+    .get("http://localhost:3333/smurfs")
+    .then(res => {
+      console.log(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
--- smurfs/src/store/reducers/index.js
+++ smurfs/src/store/reducers/index.js
@@ -1,4 +1,8 @@
-import { FETCH_SMURFS_START } from "../actions/";
+import {
+  FETCH_SMURFS_START,
+  FETCH_SMURFS_SUCCESS,
+  FETCH_SMURFS_FAILURE
+} from "../actions/";
 
 const initialState = {
   error: "",
@@ -9,7 +13,13 @@ const initialState = {
 function reducer(state = initialState, action) {
   switch (action.type) {
     case FETCH_SMURFS_START:
-      console.log("In reducer!");
+      console.log("In FETCH_START!");
+      return state;
+    case FETCH_SMURFS_START:
+      console.log("In FETCH_SUCCESS!");
+      return state;
+    case FETCH_SMURFS_START:
+      console.log("In FETCH_FAILURE!");
       return state;
     default:
       return state;
