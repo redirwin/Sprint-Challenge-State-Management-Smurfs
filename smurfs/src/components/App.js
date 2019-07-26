e0bc609eedf457d2455761b6d42e6b448597e093
Dave Irwin <david@redirwin.com>
2019-07-26 12:26:08 -0700

    Installed material-ui. Continuing styling.

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "../styles/App.css";
 
+// styling
+import Container from "@material-ui/core/Container";
+
+// app components
 import DisplaySmurfs from "./DisplaySmurfs";
+import Header from "../ui/Header";
 
 export default function App() {
   return (
-    <div className="App">
-      <DisplaySmurfs />
-    </div>
+    <>
+      <Header />
+      <Container maxWidth="sm">
+        <DisplaySmurfs />
+      </Container>
+    </>
   );
 }
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -4,14 +4,21 @@ import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./store/reducers";
+
+// styling
+import CssBaseline from "@material-ui/core/CssBaseline";
 import "./styles/index.css";
+
+// app components
 import App from "./components/App";
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <CssBaseline>
+      <App />
+    </CssBaseline>
   </Provider>,
   document.getElementById("root")
 );
