689bb6a10dba7bad4757cea7ccf6bfb9a2261431
Dave Irwin <david@redirwin.com>
2019-07-26 11:44:46 -0700

    Added form validation.

--- smurfs/src/components/AddSmurfForm.js
+++ smurfs/src/components/AddSmurfForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { withFormik, Form, Field } from "formik";
-// import * as Yup from "yup";
+import * as Yup from "yup";
 
 import { addSmurf } from "../store/actions";
 
@@ -39,6 +39,11 @@ function AddSmurfForm({ touched, errors }) {
           <button className="btn">Submit</button>
         </div>
       </Form>
+      <div className="error-group">
+        <div className="error-message">{touched.name && errors.name}</div>
+        <div className="error-message">{touched.age && errors.age}</div>
+        <div className="error-message">{touched.height && errors.height}</div>
+      </div>
     </section>
   );
 }
@@ -50,8 +55,22 @@ export default withFormik({
       height: ""
     };
   },
+
+  validationSchema: Yup.object().shape({
+    name: Yup.string()
+      .required("Please enter a name.")
+      .min(2, "Please enter a name."),
+    age: Yup.string()
+      .required("Please enter an age.")
+      .min(1, "Please enter an age."),
+    height: Yup.string()
+      .required("Please enter a height.")
+      .min(1, "Please enter a height.")
+  }),
+
   handleSubmit(values, formikBag) {
     console.log(formikBag.props);
+    formikBag.resetForm();
     formikBag.props.dispatch(addSmurf(values));
   }
 })(AddSmurfForm);
